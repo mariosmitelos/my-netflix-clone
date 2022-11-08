aea434f6d27b21b40370fe52d133409ea0438bb4
Marios Mitelos <mmitelos@deloitte.gr>
2022-11-08 11:32:10 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Row from "./Row";
+import Banner from "./Banner";
+import Nav from "./Nav";
+import HomeScreen from "./screens/HomeScreen";
+import { auth } from "./firebase";
+
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Routes,
+  Link,
+  useRouteMatch,
+} from "react-router-dom";
+import LoginScreen from "./screens/LoginScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { useDispatch, useSelector } from "react-redux";
+import { logout, login, selectUser } from "./features/userSlice";
 
 function App() {
+  const dispatch = useDispatch();
+  const user = useSelector(selectUser);
+  const { isLoggedIn } = user;
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (userAuth) => {
+      if (userAuth) {
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+
+    return () => {
+      unsubscribe;
+    };
+  }, [dispatch]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        {!isLoggedIn ? (
+          <LoginScreen />
+        ) : (
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/profile" element={<ProfileScreen />} />
+          </Routes>
+        )}
+      </BrowserRouter>
     </div>
   );
 }
